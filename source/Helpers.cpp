aa28c474e4836875a50ddd5927f3546fcf3d45b2
Lazrius <lazdisco@gmail.com>
2022-08-15 21:14:30 +0100

    Fix missing call to unix epoc

--- source/Helpers.cpp
+++ source/Helpers.cpp
@@ -2,6 +2,6 @@
 
 namespace Helpers::Time
 {
-	std::chrono::seconds Unix() { return ToSeconds(std::chrono::system_clock::now()); }
-	std::chrono::milliseconds UnixMilliseconds() { return ToMilliseconds(std::chrono::system_clock::now()); }
+	std::chrono::seconds Unix() { return ToSeconds(std::chrono::system_clock::now().time_since_epoch()); }
+	std::chrono::milliseconds UnixMilliseconds() { return ToMilliseconds(std::chrono::system_clock::now().time_since_epoch()); }
 } // namespace Helpers::Time
