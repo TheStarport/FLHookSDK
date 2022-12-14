83ba55dd665e54cd13c1e0e83651de9fc8e9fc6b
Lazrius <lazdisco@gmail.com>
2022-12-14 23:28:55 +0000

    Make default ctor for sql manager public

--- include/ext/Sql.hpp
+++ include/ext/Sql.hpp
@@ -7,13 +7,14 @@
 class DLL SqlManager : Singleton<SqlManager>
 {
 	explicit SqlManager(const std::string& path);
-	SqlManager();
 	static SQLite::Database Create(const std::string& path);
 
 	SQLite::Database db;
 	std::optional<SQLite::Transaction> activeTransaction;
 
 public:
+	SqlManager();
+
 	SQLite::Database& GetDb();
 	static std::unique_ptr<SqlManager> GetDatabase(const std::string& path);
 	SQLite::Statement StartQuery(const std::string& query, bool createTransaction = true);
