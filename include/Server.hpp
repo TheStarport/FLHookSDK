480247ecb861ba3e2d2195c1d0606fb47002c73f
Aingar <47479741+Aingar@users.noreply.github.com>
2023-01-05 14:50:35 +0100

    added GetSystemByShipId signature to Server.hpp

--- include/Server.hpp
+++ include/Server.hpp
@@ -217,6 +217,7 @@ namespace Hk
 		DLL cpp::result<ClientId, Error> GetTargetClientID(const std::variant<uint, std::wstring>& player);
 		DLL cpp::result<const BaseId, Error> GetCurrentBase(const std::variant<uint, std::wstring>& player);
 		DLL cpp::result<const SystemId, Error> GetSystem(const std::variant<uint, std::wstring>& player);
+		DLL cpp::result<const SystemId, Error> GetSystemByShipId(ShipId shipId);
 		DLL cpp::result<const ShipId, Error> GetShip(const std::variant<uint, std::wstring>& player);
 		DLL Error GetSystemByNickname(std::variant<std::string, std::wstring> nickname, uint& system);
 		DLL CShip* CShipFromShipDestroyed(const DWORD** ecx);
