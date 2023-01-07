b07677349697a2fd95804e82d72abd5a7efef64c
Aingar <47479741+Aingar@users.noreply.github.com>
2023-01-07 01:06:11 +0100

    Added Hk::Player::GetShipID

--- include/Server.hpp
+++ include/Server.hpp
@@ -218,6 +218,7 @@ namespace Hk
 		DLL cpp::result<const BaseId, Error> GetCurrentBase(const std::variant<uint, std::wstring>& player);
 		DLL cpp::result<const SystemId, Error> GetSystem(const std::variant<uint, std::wstring>& player);
 		DLL cpp::result<const ShipId, Error> GetShip(const std::variant<uint, std::wstring>& player);
+		DLL cpp::result<const uint, Error> GetShipID(const std::variant<uint, std::wstring>& player);
 		DLL Error GetSystemByNickname(std::variant<std::string, std::wstring> nickname, uint& system);
 		DLL CShip* CShipFromShipDestroyed(const DWORD** ecx);
 	} // namespace Player
