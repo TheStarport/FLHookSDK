5b8eafb76f7eb2edea485d6b6caabcebf9910537
Aingar <47479741+Aingar@users.noreply.github.com>
2023-01-18 01:05:36 +0100

    adding Hk::Time::GetUnix()

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,6 +6,8 @@ namespace Hk
 	{
 		std::chrono::seconds Unix();
 		std::chrono::milliseconds UnixMilliseconds();
+		
+		uint GetUnix();
 
 		template<typename T>
 		std::chrono::microseconds ToMicroseconds(T duration)
