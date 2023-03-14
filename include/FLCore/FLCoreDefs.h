a2696f67160c5d87fdd3946df14dbc59fe5f8baf
MrNen <mrnentendo@gmail.com>
2023-03-14 10:19:27 -0500

    Converted Matrix and Quat to GLM types.

--- include/FLCore/FLCoreDefs.h
+++ include/FLCore/FLCoreDefs.h
@@ -3,36 +3,38 @@
 //--------------------------
 //
 //	File:			FLCoreDefs.h
-//	Module:			
+//	Module:
 //	Description:	Common declarations
 //
 //	Web: www.skif.be/flcoresdk.php
-//  
+//
 //
 //////////////////////////////////////////////////////////////////////
 #ifndef _FLCOREDEFS_H_
 #define _FLCOREDEFS_H_
 
 #include "glm/glm.hpp"
+#include "glm/gtc/quaternion.hpp"
 #include "st6.h"
 
-#pragma warning(disable: 4251 4002 4099 4302)
+#pragma warning(disable : 4251 4002 4099 4302)
 
 #ifndef IMPORT
-#define IMPORT __declspec(dllimport)
+	#define IMPORT __declspec(dllimport)
 #endif
-#ifndef  EXPORT
-#define EXPORT __declspec(dllexport)
+#ifndef EXPORT
+	#define EXPORT __declspec(dllexport)
 #endif
 
-#define OBJECT_DATA_SIZE	2048
+#define OBJECT_DATA_SIZE 2048
 
-template <int size> struct TString
+template<int size>
+struct TString
 {
 	int len;
-	char data[size+1];
-	
-	TString():len(0) { data[0] = 0; }
+	char data[size + 1];
+
+	TString() : len(0) { data[0] = 0; }
 };
 
 class Vector : public glm::vec3
@@ -42,16 +44,25 @@ class Vector : public glm::vec3
 	Vector(const float a, const float b, const float c) : glm::vec3(a, b, c) {}
 };
 
-class Matrix
+class Matrix : public glm::mat3
 {
-public:
-	float data[3][3];
+  public:
+	Matrix() = default;
+	Matrix(const glm::vec3 a, const glm::vec3 b, const glm::vec3 c) : glm::mat3(a, b, c) {}
 };
 
-class Quaternion
+class Quaternion : public glm::qua<float, glm::packed_highp>
 {
-public:
-	float w,x,y,z;
+  public:
+	Quaternion() = default;
+	Quaternion(const float a, const float b, const float c, const float d) : glm::qua<float, glm::packed_highp>(a, b, c, d) {}
+	explicit Quaternion(const glm::quat& quat)
+	{
+		w = quat.w;
+		x = quat.x;
+		y = quat.y;
+		z = quat.z;
+	}
 };
 
 using uint = unsigned int;
