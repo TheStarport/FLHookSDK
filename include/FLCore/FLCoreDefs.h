1998d753070f2a969137338b57064b70d6fcf57c
Laz <lazdisco@gmail.com>
2023-06-27 22:23:48 +0100

    ﻿Run clang format on flcore common and define transform

--- include/FLCore/FLCoreDefs.h
+++ include/FLCore/FLCoreDefs.h
@@ -20,49 +20,58 @@
 #pragma warning(disable : 4251 4002 4099 4302)
 
 #ifndef IMPORT
-	#define IMPORT __declspec(dllimport)
+    #define IMPORT __declspec(dllimport)
 #endif
 #ifndef EXPORT
-	#define EXPORT __declspec(dllexport)
+    #define EXPORT __declspec(dllexport)
 #endif
 
 #define OBJECT_DATA_SIZE 2048
 
-template<int size>
+template <int size>
 struct TString
 {
-	int len;
-	char data[size + 1];
+        int len;
+        char data[size + 1];
 
-	TString() : len(0) { data[0] = 0; }
+        TString() : len(0) { data[0] = 0; }
 };
 
 class Vector : public glm::vec3
 {
-  public:
-	Vector() = default;
-	Vector(const float a, const float b, const float c) : glm::vec3(a, b, c) {}
+    public:
+        Vector() = default;
+        Vector(const float a, const float b, const float c) : glm::vec3(a, b, c) {}
 };
 
 class Matrix : public glm::mat3
 {
-  public:
-	Matrix() = default;
-	Matrix(const glm::vec3 a, const glm::vec3 b, const glm::vec3 c) : glm::mat3(a, b, c) {}
+    public:
+        Matrix() = default;
+        Matrix(const glm::vec3 a, const glm::vec3 b, const glm::vec3 c) : glm::mat3(a, b, c) {}
 };
 
 class Quaternion : public glm::qua<float, glm::packed_highp>
 {
-  public:
-	Quaternion() = default;
-	Quaternion(const float a, const float b, const float c, const float d) : glm::qua<float, glm::packed_highp>(a, b, c, d) {}
-	explicit Quaternion(const glm::quat& quat)
-	{
-		w = quat.w;
-		x = quat.x;
-		y = quat.y;
-		z = quat.z;
-	}
+    public:
+        Quaternion() = default;
+        Quaternion(const float a, const float b, const float c, const float d) : glm::qua<float, glm::packed_highp>(a, b, c, d) {}
+        explicit Quaternion(const glm::quat& quat)
+        {
+            w = quat.w;
+            x = quat.x;
+            y = quat.y;
+            z = quat.z;
+        }
+};
+
+class Transform
+{
+    public:
+        Transform() = default;
+
+        Vector pos;
+        Matrix orient;
 };
 
 using uint = unsigned int;
