ae0de449bfba8e3618ea166896a14b1c9961d55d
Laz <lazdisco@gmail.com>
2023-10-02 17:47:33 +0100

    feat: add resource ptr validation wrapper for shared/weak ptrs

---
+++ include/ResourcePtr.hpp
@@ -0,0 +1,29 @@
+template <typename T>
+class ResourcePtr
+{
+        std::weak_ptr<T> underlyingPtr;
+        std::function<bool(std::shared_ptr<T>)> validator;
+
+    public:
+        ResourcePtr(std::weak_ptr<T> ptr, const std::function<bool(std::shared_ptr<T>)>& validator) : underlyingPtr(std::move(ptr)), validator(validator) {}
+        ResourcePtr(std::shared_ptr<T> ptr, const std::function<bool(std::shared_ptr<T>)>& validator) : underlyingPtr(ptr), validator(validator) {}
+
+        // Represents a nullptr
+        ResourcePtr() {}
+
+        std::shared_ptr<T> Acquire()
+        {
+            if (!underlyingPtr)
+            {
+                return {};
+            }
+
+            auto ptr = underlyingPtr.lock();
+            if (!ptr || !validator(ptr))
+            {
+                return {};
+            }
+
+            return ptr;
+        }
+};
