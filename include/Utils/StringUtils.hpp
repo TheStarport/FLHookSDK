1a31e8bd8b13cf133b2d750d87038bed77d8782f
Laz <lazdisco@gmail.com>
2024-05-02 18:40:23 +0100

    feat: add StringHash/WStringHash structs for hashing unordered_maps with C++20

--- include/Utils/StringUtils.hpp
+++ include/Utils/StringUtils.hpp
@@ -173,6 +173,26 @@ enum class MessageFormat
     BigAndBold = Big | Bold,
 };
 
+struct StringHash
+{
+    using hash_type = std::hash<std::string_view>;
+    using is_transparent = void;
+
+    std::size_t operator()(const char* str) const        { return hash_type{}(str); }
+    std::size_t operator()(const std::string_view str) const   { return hash_type{}(str); }
+    std::size_t operator()(std::string const& str) const { return hash_type{}(str); }
+};
+
+struct WStringHash
+{
+    using hash_type = std::hash<std::wstring_view>;
+    using is_transparent = void;
+
+    std::size_t operator()(const wchar_t* str) const        { return hash_type{}(str); }
+    std::size_t operator()(const std::wstring_view str) const   { return hash_type{}(str); }
+    std::size_t operator()(std::wstring const& str) const { return hash_type{}(str); }
+};
+
 class StringUtils
 {
     public:
