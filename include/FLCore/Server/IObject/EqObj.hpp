a233185bd39158280ac6487e70fadd2ee926eda5
Lazrius <lazdisco@gmail.com>
2025-10-05 15:08:37 +0100

    fix: rename unlight fuse and fix signature

--- include/FLCore/Server/IObject/EqObj.hpp
+++ include/FLCore/Server/IObject/EqObj.hpp
@@ -6,23 +6,23 @@
 struct EqObj : public GameObject
 {
 
-        virtual void sub_6CE7D00();                         // collision groups, sets off fuses?                                                //444
-        virtual void sub_6CE8C50();                         // damage dealing method, sets off death animation?                                 //448
-        virtual void sub_6CE8D40();                         // same as above, but for collision groups                                          //452
-        virtual void sub_6CE8CD0();                         // collision group related                                                          //456
-        virtual void sub_6CE8E90();                         // hardpoint eq related                                                             //460
-        virtual void sub_6CE8E10();                         // hardpoint eq related                                                             //464
+        virtual void sub_6CE7D00(); // collision groups, sets off fuses?                                                //444
+        virtual void sub_6CE8C50(); // damage dealing method, sets off death animation?                                 //448
+        virtual void sub_6CE8D40(); // same as above, but for collision groups                                          //452
+        virtual void sub_6CE8CD0(); // collision group related                                                          //456
+        virtual void sub_6CE8E90(); // hardpoint eq related                                                             //460
+        virtual void sub_6CE8E10(); // hardpoint eq related                                                             //464
         virtual bool drop_all_cargo(const char* hardpoint, DamageList*); // iterate over cargo, dunno                                //468
-        virtual bool hit_shield_bubble(DamageList*);        // 472
-        virtual void sub_6CE88D0();                         // iterate over all equipped equipment and run an update?                           //476
-        virtual void sub_6CE8930();                         // processes values under 0x74 and 0x78 pointers                                    //480
-        virtual bool light_fuse(uint dunno, const Id& fuseId, ushort sId, float radius, float fuseLifetime);                                    // 484 sub_6D01A90
-        virtual bool unlight_fuse_unk(Id fuseId, ushort sid, float lifeTime);                                                               // 488 sub_6CEC7F0
-        virtual bool fuse_expiration_check();                                                                                                 // 492 sub_6CEC8D0
-        virtual void death_explosion();                                                                                                       // 496 sub_6CE8400
-        virtual void sub_6D01C90();                                                                                                           // 500
-        virtual void sub_6CE8760();                                                                                                           // 504
-        virtual void process_explosion_damage_hull(ExplosionDamageEvent* explosion, DamageList* dmgList);                                     // 508 sub_6CE9550
+        virtual bool hit_shield_bubble(DamageList*);                     // 472
+        virtual void sub_6CE88D0(); // iterate over all equipped equipment and run an update?                           //476
+        virtual void sub_6CE8930(); // processes values under 0x74 and 0x78 pointers                                    //480
+        virtual bool light_fuse(uint dunno, const Id& fuseId, ushort sId, float radius, float fuseLifetime);                     // 484 sub_6D01A90
+        virtual bool unlight_fuse(const Id& fuseId, ushort sid, float lifeTime);                                                 // 488 sub_6CEC7F0
+        virtual bool fuse_expiration_check();                                                                                    // 492 sub_6CEC8D0
+        virtual void death_explosion();                                                                                          // 496 sub_6CE8400
+        virtual void sub_6D01C90();                                                                                              // 500
+        virtual void sub_6CE8760();                                                                                              // 504
+        virtual void process_explosion_damage_hull(ExplosionDamageEvent* explosion, DamageList* dmgList);                        // 508 sub_6CE9550
         virtual void process_explosion_damage_external_equip_and_col_grps(ExplosionDamageEvent* explosion, DamageList* dmgList); // 512 sub_6CE9690
         virtual bool process_explosion_damage_shield_bubble(ExplosionDamageEvent* explosion, DamageList* dmgList); // NakedGuidedHit   //516 sub_6CE9A90
         virtual void process_explosion_damage_energy(ExplosionDamageEvent* explosion, DamageList* dmgList);        // 520 sub_6CE9940
