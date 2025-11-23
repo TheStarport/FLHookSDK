21720b31158070ad844f2b6b077e62f3ee28f0c5
Aingar <47479741+Aingar@users.noreply.github.com>
2025-11-23 12:10:39 +0100

    feat: add bool-less INI_Reader helper method overloads

--- include/FLCore/Common/INI_Reader.hpp
+++ include/FLCore/Common/INI_Reader.hpp
@@ -59,6 +59,12 @@ class INI_Reader
             return true;
         }
 
+        bool GetFloat(const std::string_view key, float& value, const int param = 0)
+        {
+            bool dummy;
+            return GetFloat(key, value, dummy, param);
+        }
+
         bool GetInt(const std::string_view key, int& value, bool& found, const int param = 0)
         {
             if (!is_value(key.data()))
@@ -69,6 +75,12 @@ class INI_Reader
             found = true;
             value = get_value_int(param);
             return true;
+        }
+
+        bool GetInt(const std::string_view key, int& value, const int param = 0)
+        {
+            bool dummy;
+            return GetInt(key, value, dummy, param);
         }
 
         bool GetString(const std::string_view key, std::string& value, bool& found, const int param = 0)
@@ -83,6 +95,12 @@ class INI_Reader
             return true;
         }
 
+        bool GetWString(const std::string_view key, std::string& value, const int param = 0)
+        {
+            bool dummy;
+            return GetString(key, value, dummy, param);
+        }
+
         bool GetWString(const std::string_view key, std::wstring& value, bool& found, const int param = 0)
         {
             if (!is_value(key.data()))
@@ -92,6 +110,12 @@ class INI_Reader
             found = true;
             value = StringUtils::stows(get_value_string(param));
             return true;
+        }
+
+        bool GetWString(const std::string_view key, std::wstring& value, const int param = 0)
+        {
+            bool dummy;
+            return GetWString(key, value, dummy, param);
         }
 
         bool GetHash(const std::string_view key, uint& value, bool& found, const int param = 0)
@@ -104,6 +128,12 @@ class INI_Reader
             found = true;
             value = CreateID(get_value_string(param));
             return true;
+        }
+
+        bool GetHash(const std::string_view key, uint& value, const int param = 0)
+        {
+            bool dummy;
+            return GetHash(key, value, dummy, param);
         }
 
         st6::string fileName;
