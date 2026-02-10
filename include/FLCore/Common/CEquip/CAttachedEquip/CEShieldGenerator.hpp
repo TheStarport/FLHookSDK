df8be69eff9a046ce66b120ad7a1911599331c3a
Lazrius <lazdisco@gmail.com>
2026-02-10 00:23:31 +0000

    chore: fix missing includes for CEPower and CEShieldGenerator

--- include/FLCore/Common/CEquip/CAttachedEquip/CEShieldGenerator.hpp
+++ include/FLCore/Common/CEquip/CAttachedEquip/CEShieldGenerator.hpp
@@ -1,21 +1,23 @@
 #pragma once
 #include "../../../FLCoreDefs.hpp"
 
+#include "FLCore/Common/CEquip/CAttachedEquip.hpp"
+
 class IMPORT CEShieldGenerator : public CAttachedEquip
 {
     public:
-    virtual ~CEShieldGenerator();
-    virtual bool IsFunctioning() const;
-    virtual bool Update(float, unsigned int);
-    virtual void Destroy();
+        virtual ~CEShieldGenerator();
+        virtual bool IsFunctioning() const;
+        virtual bool Update(float, unsigned int);
+        virtual void Destroy();
 
-    static CEShieldGenerator* cast(CEquip*);
-    static const CEShieldGenerator* cast(const CEquip*);
+        static CEShieldGenerator* cast(CEquip*);
+        static const CEShieldGenerator* cast(const CEquip*);
 
-    CEShieldGenerator(const CEShieldGenerator&);
-    CEShieldGenerator(CEqObj*, unsigned short, const Archetype::ShieldGenerator*, bool);
-    const Archetype::ShieldGenerator* ShieldGenArch() const;
-    bool IsLinked() const;
-    void LinkShield(CEShield*);
-    void UnLinkShield();
+        CEShieldGenerator(const CEShieldGenerator&);
+        CEShieldGenerator(CEqObj*, unsigned short, const Archetype::ShieldGenerator*, bool);
+        const Archetype::ShieldGenerator* ShieldGenArch() const;
+        bool IsLinked() const;
+        void LinkShield(CEShield*);
+        void UnLinkShield();
 };
--- include/FLCore/Common/CEquip/CEPower.hpp
+++ include/FLCore/Common/CEquip/CEPower.hpp
@@ -1,20 +1,25 @@
 #pragma once
 #include "../../FLCoreDefs.hpp"
 
+namespace Archetype
+{
+    struct Power;
+}
+
 class CEPower : public CEquip
 {
     public:
-    IMPORT virtual ~CEPower();
-    IMPORT virtual bool Update(float, unsigned int);
+        IMPORT virtual ~CEPower();
+        IMPORT virtual bool Update(float, unsigned int);
 
-    IMPORT static CEPower* cast(CEquip*);
-    IMPORT static const CEPower* cast(const CEquip*);
+        IMPORT static CEPower* cast(CEquip*);
+        IMPORT static const CEPower* cast(const CEquip*);
 
-    IMPORT CEPower(const CEPower&);
-    IMPORT CEPower(CEqObj*, unsigned short, const Archetype::Power*, bool);
+        IMPORT CEPower(const CEPower&);
+        IMPORT CEPower(CEqObj*, unsigned short, const Archetype::Power*, bool);
 
-    IMPORT float GetCapacity() const;
-    IMPORT float GetChargeRate() const;
-    IMPORT float GetThrustCapacity() const;
-    IMPORT float GetThrustChargeRate() const;
+        IMPORT float GetCapacity() const;
+        IMPORT float GetChargeRate() const;
+        IMPORT float GetThrustCapacity() const;
+        IMPORT float GetThrustChargeRate() const;
 };
