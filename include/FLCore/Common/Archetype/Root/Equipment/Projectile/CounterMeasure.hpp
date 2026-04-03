0db5f10a6f95342c4f0774eaf595c21643f16587
IrateRedKite <RupertCW@gmail.com>
2026-04-03 18:55:55 +0100

    fix: missing headers in projectile includes

--- include/FLCore/Common/Archetype/Root/Equipment/Projectile/CounterMeasure.hpp
+++ include/FLCore/Common/Archetype/Root/Equipment/Projectile/CounterMeasure.hpp
@@ -1,22 +1,22 @@
 #pragma once
 #include "FLCore/FLCoreDefs.hpp"
-
+#include "../Projectile.hpp"
 namespace Archetype
 {
     struct CounterMeasure : Projectile
     {
-        IMPORT CounterMeasure(const CounterMeasure&);
-        IMPORT CounterMeasure(IClObj*);
-        IMPORT virtual ~CounterMeasure();
-        IMPORT CounterMeasure& operator=(const CounterMeasure&);
-        IMPORT virtual ClassType get_class_type() const;
-        IMPORT virtual HpAttachmentType get_hp_type() const;
-        IMPORT virtual bool read(INI_Reader&);
-        IMPORT virtual void redefine(const Root&);
+            IMPORT CounterMeasure(const CounterMeasure&);
+            IMPORT CounterMeasure(IClObj*);
+            IMPORT virtual ~CounterMeasure();
+            IMPORT CounterMeasure& operator=(const CounterMeasure&);
+            IMPORT virtual ClassType get_class_type() const;
+            IMPORT virtual HpAttachmentType get_hp_type() const;
+            IMPORT virtual bool read(INI_Reader&);
+            IMPORT virtual void redefine(const Root&);
 
         public:
-        /* 32 */ float linearDrag;
-        /* 33 */ float range;
-        /* 34 */ unsigned diversionPercentage;
+            /* 32 */ float linearDrag;
+            /* 33 */ float range;
+            /* 34 */ unsigned diversionPercentage;
     };
-}
+} // namespace Archetype
