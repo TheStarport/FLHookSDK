5039f1aec9b4089be02756c420e8c77983355422
Aingar <47479741+Aingar@users.noreply.github.com>
2026-04-29 16:36:50 +0200

    feat: map out CBeam

--- include/FLCore/Common/CObjects/CSimple/CProjectile/CBeam.hpp
+++ include/FLCore/Common/CObjects/CSimple/CProjectile/CBeam.hpp
@@ -4,17 +4,24 @@
 struct CBeam : public CProjectile
 {
     public:
-    IMPORT virtual ~CBeam();
-    IMPORT virtual Vector get_velocity() const;
-    IMPORT virtual f32 get_mass() const;
-    IMPORT virtual void init_physics(const Vector&, const Vector&);
-    IMPORT virtual void expire_safe_time();
+        IMPORT virtual ~CBeam();
+        IMPORT virtual Vector get_velocity() const;
+        IMPORT virtual f32 get_mass() const;
+        IMPORT virtual void init_physics(const Vector&, const Vector&);
+        IMPORT virtual void expire_safe_time();
 
-    IMPORT CBeam(const CBeam&);
-    IMPORT CBeam(Class);
-    IMPORT const char* get_collision_group() const;
-    IMPORT const Vector& get_last_pos() const;
-    IMPORT void move(f32);
-    IMPORT const Archetype::Munition* munitionarch() const;
-    IMPORT void set_velocity(const Vector&);
+        IMPORT CBeam(const CBeam&);
+        IMPORT CBeam(Class);
+        IMPORT const char* get_collision_group() const;
+        IMPORT const Vector& get_last_pos() const;
+        IMPORT void move(f32);
+        IMPORT const Archetype::Munition* munitionarch() const;
+        IMPORT void set_velocity(const Vector&);
+
+        bool safeTimeExpired;    // 60
+        unsigned int beamUnused; // 61
+        Vector velocity;         // 62
+        Vector initialPosition;  // 65
+        Vector lastPosition;     // 68
+        bool initialized;        // 69
 };
