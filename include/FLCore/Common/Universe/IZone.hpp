5e6de16696fca02034e39f51102b74b1f9f3476e
Aingar <47479741+Aingar@users.noreply.github.com>
2026-05-02 11:53:20 +0200

    feat: reverse engineered a few Universe::IZone methods

--- include/FLCore/Common/Universe/IZone.hpp
+++ include/FLCore/Common/Universe/IZone.hpp
@@ -11,6 +11,44 @@ namespace Universe
 {
     struct IZone
     {
+        virtual void dunno0();
+        virtual void dunno4();
+        virtual void dunno8();
+        virtual void dunnoC();
+        virtual void dunno10();
+        virtual void dunno14();
+        virtual void dunno18();
+        virtual void dunno1C();
+        virtual void dunno20();
+        virtual void dunno24();
+        virtual void dunno28();
+        virtual void dunno2C();
+        virtual void dunno30();
+        virtual bool is_within_zone(const Vector& pos, float objRadius) const;
+        virtual bool is_within_zone2D(const Vector& pos, float objRadius) const;
+        virtual void dunno3C();
+        virtual void dunno40();
+        virtual void dunno44();
+        virtual float get_immersion(const Vector& pos) const;
+        virtual void dunno4C();
+        virtual void dunno50();
+        virtual void dunno54();
+        virtual void dunno58();
+        virtual void dunno5C();
+        virtual void dunno60();
+        virtual void dunno64();
+        virtual void dunno68();
+        virtual void dunno6C();
+        virtual void dunno70();
+        virtual void dunno74();
+        virtual void dunno78();
+        virtual void dunno7C();
+        virtual void dunno80();
+        virtual void dunno84();
+        virtual void dunno88();
+        virtual void dunno8C();
+        virtual void dunno90();
+
         struct FactionSpawn
         {
             unsigned faction; // Faction Id, obtainable with e.g. pub::Reputation::GetReputationGroup()
@@ -27,7 +65,6 @@ namespace Universe
             Mesh
         };
 
-        void* vtable;
         Id zoneId;
         SystemId systemId;
         Matrix orientation;
